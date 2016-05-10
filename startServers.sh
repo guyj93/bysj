2d414da2a1fc8a36e77a282c82c6c9f074a17aca
eyujugu <yujun.gu@ericsson.com>
2016-05-11 04:03:11 +0800

    change redis.conf to disable persistence and limit memory usage

--- startServers.sh
+++ startServers.sh
@@ -2,6 +2,6 @@ scriptPath=$(cd $(/usr/bin/dirname "${BASH_SOURCE[0]}");pwd;)
 echo "will start netperf, iperf3, redis-server and netLatencyTester servers in daemon mode"
 /usr/local/bin/netserver -p 2865
 /usr/bin/iperf3 -s -D
-/usr/local/bin/redis-server $scriptPath/redis.conf
+/usr/local/bin/redis-server $scriptPath/redis/redis.conf
 $scriptPath/netLatencyTester/netLatencyTester -s -laddr ":1234" -cpus 1 > /dev/null &
 sleep 1
