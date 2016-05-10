b0931e8986aa57c2afa019f800eac9f254f8ae6e
eyujugu <yujun.gu@ericsson.com>
2016-05-11 04:51:08 +0800

    fix bug: wrong path of testTools

--- startServers.sh
+++ startServers.sh
@@ -1,7 +1,7 @@
 scriptPath=$(cd $(/usr/bin/dirname "${BASH_SOURCE[0]}");pwd;)
-echo "will start netperf, iperf3, redis-server and netLatencyTester servers in daemon mode"
+echo "will start netperf, iperf3, redis-server and netLatencyTester servers"
 /usr/local/bin/netserver -p 2865
 /usr/bin/iperf3 -s -D
-/usr/local/bin/redis-server $scriptPath/redis/redis.conf
-$scriptPath/netLatencyTester/netLatencyTester -s -laddr ":1234" -cpus 1 > /dev/null &
+/usr/local/bin/redis-server $scriptPath/testTools/redis/redis.conf
+$scriptPath/testTools/netLatencyTester/netLatencyTester -s -laddr ":1234" -cpus 1 > /dev/null &
 sleep 1
