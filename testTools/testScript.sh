f1ece330918f32ec62ae08e516ff6eb4382524ac
eyujugu <yujun.gu@ericsson.com>
2016-05-11 21:43:59 +0800

    better names  of result files

--- testTools/testScript.sh
+++ testTools/testScript.sh
@@ -13,23 +13,23 @@ if [ $# -eq "5" ]; then
 	scriptPath=$(cd $(dirname "${BASH_SOURCE[0]}");pwd;)
 	
 	echo "start netperf"
-	netperf -c -C -l 60 -H $serverAddr -p $netperfPort -t TCP_RR -- -r 100,200 -P ",$(expr ${netperfPort} + 1)"> "${pwd}/${serverAddr}_${netperfPort}_netperf.txt";	
+	netperf -c -C -l 60 -H $serverAddr -p $netperfPort -t TCP_RR -- -r 100,200 -P ",$(expr ${netperfPort} + 1)"> "${pwd}/netperf_${serverAddr}:${netperfPort}.txt";	
 
 	echo "start iperf3"
-	iperf3 -c $serverAddr -p $iperfPort > "${pwd}/${serverAddr}_${iperfPort}_iperf.txt";
+	iperf3 -c $serverAddr -p $iperfPort > "${pwd}/iperf3_${serverAddr}:${iperfPort}.txt";
 
 	echo "start redis-benchmark"
 	redis-cli -h $serverAddr -p $redisPort flushall > /dev/null
-	redis-benchmark -h $serverAddr -p $redisPort --csv > "${pwd}/${serverAddr}_${redisPort}_redis.txt";
+	redis-benchmark -h $serverAddr -p $redisPort --csv > "${pwd}/redis_${serverAddr}:${redisPort}.txt";
 
 	echo "start testChangeRequestSize"
-	${scriptPath}/netLatencyTester/testChangeRequestSize.sh -q -ad "${serverAddr}:${latencyTesterPort}" -r 1000 -rp 1ms -cpus 1 > "${pwd}/${serverAddr}_${latencyTesterPort}_changeRequestSize.txt";
+	${scriptPath}/netLatencyTester/testChangeRequestSize.sh -q -ad "${serverAddr}:${latencyTesterPort}" -r 1000 -rp 1ms -cpus 1 > "${pwd}/changeRequestSize_${serverAddr}:${latencyTesterPort}.txt";
 
 	echo "start testChangeRequestPeriod"
-	${scriptPath}/netLatencyTester/testChangeRequestPeriod.sh -q -ad "${serverAddr}:${latencyTesterPort}" -r 1000 -cpus 1 > "${pwd}/${serverAddr}_${latencyTesterPort}_changeRequestPeriod.txt";
+	${scriptPath}/netLatencyTester/testChangeRequestPeriod.sh -q -ad "${serverAddr}:${latencyTesterPort}" -r 1000 -cpus 1 > "${pwd}/changeRequestPeriod_${serverAddr}:${latencyTesterPort}.txt";
 	
 	echo "start large sample test"
-	${scriptPath}/netLatencyTester/netLatencyTester -ad "${serverAddr}:${latencyTesterPort}" -rp 1ms -r 100000 -cpus 1 -fc "${pwd}/${serverAddr}:${latencyTesterPort}_largeSample_conn.txt" -fr "${pwd}/${serverAddr}:${latencyTesterPort}_largeSample_rtt.txt"> "${pwd}/${serverAddr}_${latencyTesterPort}_largeSample.txt";
+	${scriptPath}/netLatencyTester/netLatencyTester -ad "${serverAddr}:${latencyTesterPort}" -rp 1ms -r 100000 -cpus 1 -fc "${pwd}/largeSample_conn_${serverAddr}:${latencyTesterPort}.txt" -fr "${pwd}/largeSample_rtt_${serverAddr}:${latencyTesterPort}.txt"> "${pwd}/largeSample_${serverAddr}:${latencyTesterPort}.txt";
 
 else
 	echo "usage: testScript.sh serverAddr iperfPort redisPort netLatencyTesterPort"
