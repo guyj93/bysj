5ddc0ddbaf94455789c6afe48856ed15c28bc96c
eyujugu <yujun.gu@ericsson.com>
2016-05-13 18:44:13 +0800

    remove cpu statistic in netperf test, add wait response test of netLatencyTester

--- testTools/testScript.sh
+++ testTools/testScript.sh
@@ -13,7 +13,7 @@ if [ $# -eq "5" ]; then
 	scriptPath=$(cd $(dirname "${BASH_SOURCE[0]}");pwd;)
 	
 	echo "start netperf"
-	netperf -c -C -l 60 -H $serverAddr -p $netperfPort -t TCP_RR -- -r 100,200 -P ",$(expr ${netperfPort} + 1)"> "${pwd}/netperf_${serverAddr}_${netperfPort}.txt";	
+	netperf -l 60 -H $serverAddr -p $netperfPort -t TCP_RR -- -r 100,200 -P ",$(expr ${netperfPort} + 1)" > "${pwd}/netperf_${serverAddr}_${netperfPort}.txt";
 
 	echo "start iperf3"
 	iperf3 -c $serverAddr -p $iperfPort > "${pwd}/iperf3_${serverAddr}_${iperfPort}.txt";
@@ -29,8 +29,10 @@ if [ $# -eq "5" ]; then
 	${scriptPath}/netLatencyTester/testChangeRequestPeriod.sh -q -ad "${serverAddr}:${latencyTesterPort}" -r 1000 -cpus 1 > "${pwd}/changeRequestPeriod_${serverAddr}_${latencyTesterPort}.txt";
 	
 	echo "start large sample test"
-	${scriptPath}/netLatencyTester/netLatencyTester -ad "${serverAddr}:${latencyTesterPort}" -rp 1ms -r 100000 -cpus 1 -fc "${pwd}/largeSample_conn_${serverAddr}_${latencyTesterPort}.txt" -fr "${pwd}/largeSample_rtt_${serverAddr}_${latencyTesterPort}.txt"> "${pwd}/largeSample_${serverAddr}_${latencyTesterPort}.txt";
+	${scriptPath}/netLatencyTester/netLatencyTester -ad "${serverAddr}:${latencyTesterPort}" -rp 1ms -r 100000 -cpus 1 -fc "${pwd}/largeSample_conn_${serverAddr}_${latencyTesterPort}.txt" -fr "${pwd}/largeSample_rtt_${serverAddr}_${latencyTesterPort}.txt" > "${pwd}/largeSample_${serverAddr}_${latencyTesterPort}.txt";
 
+	echo "start wait response test"
+	${scriptPath}/netLatencyTester/netLatencyTester -ad "${serverAddr}:${latencyTesterPort}" -wr -rp 0ms -r 100000 -cpus 1 -fc "${pwd}/waitResponse_conn_${serverAddr}_${latencyTesterPort}.txt" -fr "${pwd}/waitResponse_rtt_${serverAddr}_${latencyTesterPort}.txt" > "${pwd}/waitResponse_${serverAddr}_${latencyTesterPort}.txt";
 else
 	echo "usage: testScript.sh serverAddr iperfPort redisPort netLatencyTesterPort"
 	echo "eg. testScript.sh 127.0.0.1 2865 5201 6379 1234"
